19551ca5e84eeb5c4469a54677a4c4f913e893df
Gagun45 <selyanchyn45@gmail.com>
2026-05-15 23:22:55 +0300

    cors

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 export default async function Home() {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  console.log(API_URL);
   const res = await fetch(`${API_URL}/api/hello`, { next: { revalidate: 15 } });
   const data = await res.json();
   return (
