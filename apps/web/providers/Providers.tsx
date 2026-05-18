7770022e362f20f9f33f75bb101b6b832bccddb6
Gagun45 <selyanchyn45@gmail.com>
2026-05-18 11:00:23 +0300

    themes added

--- apps/web/providers/Providers.tsx
+++ apps/web/providers/Providers.tsx
@@ -1,8 +1,20 @@
 import { ReactNode } from "react";
 import QueryProvider from "./QueryProvider";
+import { ThemeProvider } from "./ThemeProvider";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <QueryProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </QueryProvider>
+  );
 };
 
 export default Providers;
