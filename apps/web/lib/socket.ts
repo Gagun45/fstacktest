da802ceb571508e8060edcb6620055376d487add
Gagun45 <selyanchyn45@gmail.com>
2026-06-08 19:25:16 +0300

    socket url changed

--- apps/web/lib/socket.ts
+++ apps/web/lib/socket.ts
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 // Connect to your Express server URL
-const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false, // We will connect manually
